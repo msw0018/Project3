7448c536d6f9c7b69dc0a082b2ae1797ba8d2a51
msw0018 <36931550+msw0018@users.noreply.github.com>
2018-04-20 21:21:26 -0400

    Add files via upload

---
+++ ImageProcessing.java
@@ -0,0 +1,108 @@
+import java.awt.Color;
+
+public class ImageProcessing {
+	
+	public static void main(String[] args) {
+		//Create new image utility.
+		ImageUtils utils = new ImageUtils();
+		
+		// Load in an image.
+		Color[][] orig = utils.loadImage("src/LennaCV.png");
+		
+		// Add original image tab.
+		utils.addImage(orig, "Original image.");
+		
+		// Process the image.
+		Color[][] checker = process(orig);
+		Color[][] blues = inverse(orig);
+		Color[][] pixelly = blackWhite(orig);
+		Color[][] grey = blackAndWhite(orig);
+		
+		utils.addImage(checker, "Checkers");
+		utils.addImage(blues, "Inverse");
+		utils.addImage(pixelly, "Black and White");
+		utils.addImage(grey, "Grey Scale");
+		
+		// Display the images.
+		utils.display();
+	}
+	
+	public static Color[][] blackWhite(Color[][] img){
+		Color[][] tmp = ImageUtils.cloneArray(img);
+		
+		for (int i = 0; i < tmp.length; i++) {
+			for (int j = 0; j < tmp[i].length; j++) {
+				Color pixel = tmp[i][j];
+				int r = pixel.getRed();
+				int g = pixel.getGreen();
+				int b = pixel.getBlue();
+				
+				if (r > 95 && b > 95 && g > 95) {
+					r = 255;
+					b = 255;
+					g = 255;
+				}
+				else {
+					r = 0;
+					b = 0;
+					g = 0;
+				}
+				tmp[i][j] = new Color(r, b, g);
+			}
+		}
+		return tmp;
+	}
+	
+	public static Color[][] inverse(Color[][] img){
+		Color[][] tmp = ImageUtils.cloneArray(img);
+		for (int i = 0; i < tmp.length; i++) {
+			for (int j = 0; j < tmp[i].length; j++) {
+				Color pixel = tmp[i][j];
+				int r = pixel.getRed();
+				int g = pixel.getGreen();
+				int b = pixel.getBlue();
+				
+				tmp[i][j] = new Color(255-r, 255-b, 255-g);
+			}
+		}	
+		
+		return tmp;
+	}
+	
+	public static Color[][] process(Color[][] img){
+		Color[][] tmp = ImageUtils.cloneArray(img);
+		
+		for (int i = 0; i < tmp.length; i++) {
+			for (int j = 0; j < tmp[i].length; j++) {
+				// Red, Green, Blue: 0-255
+				Color pixel = tmp[i][j];
+				int r = pixel.getRed();
+				int g = pixel.getGreen();
+				int b = pixel.getBlue();
+				
+                if ( (i/74 % 2) == (j/74 % 2) )
+                    
+                tmp[i][j] = new Color(r/2, b/2, g/2);
+
+			}
+		}
+		
+		return tmp;
+	}
+	
+	public static Color[][] blackAndWhite(Color[][] img){
+		Color[][] tmp = ImageUtils.cloneArray(img);
+		
+		for (int i = 0; i < tmp.length; i++) {
+			for (int j = 0; j < tmp[i].length; j++) {
+				Color pixel = tmp[i][j];
+				int r = pixel.getRed();
+				int g = pixel.getGreen();
+				int b = pixel.getBlue();
+				tmp[i][j] = new Color((r+b+g)/3, (r+b+g)/3, (r+b+g)/3);
+			}
+		}
+		return tmp;
+	}
+	
+}
